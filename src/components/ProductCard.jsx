df1fd68fcf3a9bb5dfeb4142a31a7af34d06cd7b
Yeboah Michael <ymikenzy55@gmail.com>
2026-04-23 02:09:49 +0000

    Fixed Image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ( { product }) => {
 
   return (
     <div className="product-card">
-              <img src={product.image} className="product-card-image"/>
+              <img src={product.image} className="product-card-image" alt={product.name}/>
               <div className="product-card-content">
                 <h3 className="product-card-name">{product.name}</h3>
                 <p className="product-card-price">₵{product.price}</p>
